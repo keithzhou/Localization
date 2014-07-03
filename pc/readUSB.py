63ce1a2c132498dfb5598ac9259696b275a02e7b
Gemy Zhou <gemy.zhou@gmail.com>
2014-07-03 14:59:51 -0400

    update saveing audio file, not working yet

--- pc/readUSB.py
+++ pc/readUSB.py
@@ -11,19 +11,21 @@ def printUSB():
     ser = serial.Serial(USBPORTNAME, USBBAUDRATE)
     waveform = list()
     try:
+        ts = time.time()
         while True:
             data = int(ser.readline())
             waveform.append(data)
-            print "%d" % data
+            print "%d dt:%.6f" % (data, time.time() - ts)
+            ts = time.time()
     except:
-        print "Error"
+        print "save audio file"
         waveform = np.array(waveform) * 1.0
+        print "mean:%.4f" % np.mean(waveform)
         waveform -= np.mean(waveform)
-        waveform /= np.max(np.abs(waveform))
-        waveform *= 32767
-        scipy.io.wavfile.write('test.wav',44100,scaled)
-        
-
+        waveform = np.int16((waveform/np.max(np.abs(waveform))) * 32767)
+        print waveform[:100]
+        print "max:%d min:%d avg:%.4f" % (np.max(waveform), np.min(waveform), np.mean(waveform))
+        scipy.io.wavfile.write('test.wav',100,waveform)
 
 if __name__ == "__main__":
     printUSB()
