af4219146f3ee18fd1a8dd8b15c92b39920aa2f9
keith <n/a>
2014-07-03 14:29:30 -0400

    change printUSB save to file

--- pc/readUSB.py
+++ pc/readUSB.py
@@ -1,6 +1,29 @@
 import serial
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import scipy.io.wavfile
 
-ser = serial.Serial('/dev/tty.usbmodem406541',9600)
-while True:
-	print ser.readline()
+USBPORTNAME = '/dev/tty.usbmodem406541'
+USBBAUDRATE = 9600
 
+def printUSB():
+    ser = serial.Serial(USBPORTNAME, USBBAUDRATE)
+    waveform = list()
+    try:
+        while True:
+            data = int(ser.readline())
+            waveform.append(data)
+            print "%d" % data
+    except:
+        print "Error"
+        waveform = np.array(waveform) * 1.0
+        waveform -= np.mean(waveform)
+        waveform /= np.max(np.abs(waveform))
+        waveform *= 32767
+        scipy.io.wavfile.write('test.wav',44100,scaled)
+        
+
+
+if __name__ == "__main__":
+    printUSB()
