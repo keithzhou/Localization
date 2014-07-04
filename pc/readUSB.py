1. change timing function 2. fix waveform normalization 3. plot data

pc/readUSB.py
@@ -2,6 +2,7 @@ import serial
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+import datetime
 import scipy.io.wavfile
 
 USBPORTNAME = '/dev/tty.usbmodem406541'
@@ -11,16 +12,22 @@ def printUSB():
     ser = serial.Serial(USBPORTNAME, USBBAUDRATE)
     waveform = list()
     try:
-        ts = time.time()
+        ts = datetime.datetime.now()
         while True:
             data = int(ser.readline())
             waveform.append(data)
-            print "%d dt:%.6f" % (data, time.time() - ts)
-            ts = time.time()
+            ttmp = datetime.datetime.now()
+            print "data:%d since_last_message::%.6f" % (data, (ttmp - ts).microseconds/1e6)
+            ts = ttmp
     except:
         waveform = np.array(waveform).astype(np.float) 
-        waveform = (((waveform - np.mean(waveform)) / np.max(np.abs(waveform))) * 10000).astype(np.int16)
-        scipy.io.wavefile.write('output.wav',44100,waveform) # writing the sound to a file
+        waveform -= np.mean(waveform)
+        waveform = (waveform / np.max(np.abs(waveform)) * 10000).astype(np.int16)
+        fig = plt.figure()
+        plt.plot(waveform)
+        plt.savefig('output.png')
+        print waveform.shape
+        scipy.io.wavfile.write('output.wav',1000,waveform) # writing the sound to a file
         print "Done Saving"
 
 if __name__ == "__main__":