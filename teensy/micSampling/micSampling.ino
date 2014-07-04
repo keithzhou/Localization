b38edccb0619def4c150e867dfb30b72d1462ca4
keith <n/a>
2014-07-04 16:12:18 -0400

    added teensy code

---
+++ teensy/micSampling/micSampling.ino
@@ -0,0 +1,29 @@
+/*
+  AnalogReadSerial
+  Reads an analog input on pin 0, prints the result to the serial monitor.
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+ 
+ This example code is in the public domain.
+ */
+elapsedMicros sinceLastRead;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  if (sinceLastRead > 1000 * 1) {
+    sinceLastRead = 0;
+
+    Serial.println(10);  
+  }
+}
+
+
+    // read the input on analog pin 0:
+    //int sensorValue = analogRead(A0);
+    // print out the value you read:
+    //Serial.println(sensorValue);
